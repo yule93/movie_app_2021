562cd74906c30fc54b15b6f27f20858f1ca3a34a
yule93 <dbfm2222@gmail.com>
2021-01-15 20:45:52 +0900

    뭐야 이거 왜 이래ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <HashRouter>
+  return (  // Link must be inside HashRouter
+  <HashRouter>
+    <Navigation />   
     <Route path="/" exact={true} component = {Home} />
     <Route path="/about" component = {About} />
   </HashRouter>
+  );
 }
 
 export default App;
