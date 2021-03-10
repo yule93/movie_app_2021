318e29c755b541f6b1706a7958c8ff69e5a22c67
yule93 <dbfm2222@gmail.com>
2021-03-10 18:14:26 +0900

    fix the error which occured in Home.js, Movie.js by collapsing 'map' method.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,51 +1,50 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 import Movie from "../components/Movie";
 import "./Home.css";
 
-class Home extends React.Component {    // class component
-
+class Home extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
-
-  getMovies = async () => {   // async를 사용해야 await를 사용할 수 있다. async는 비동기를 의미.
+  getMovies = async () => {
     const {
       data: {
         data: { movies }
       }
-    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
     this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
-
-  render() {    // automatically compile, 'class' html tag attribute is confused with 'Home class'
+  render() {
     const { isLoading, movies } = this.state;
     return (
-    <section className = "container">
-      {isLoading ? (<div className="loader">
-          <span className = "loader__text">Loading...</span>
-        </div>
-      ) : (
-        <div className = "movies">
-          {movies.map( movie => {
-            console.log(movie);
-            return <Movie
-              key = {movie.id}
-              id = {movie.id}
-              year = {movie.year}
-              title = {movie.title}
-              summary = {movie.summary}
-              poster = {movie.medium_cover_image}
-              genres = {movie.genres}
-            />
-        })}
-        </div>
-      )}
-    </section>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
